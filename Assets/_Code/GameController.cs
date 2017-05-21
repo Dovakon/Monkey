632dc01f4193ea4ee902c7662508f412c428bcde
Anestis Konstantinidis <anestiskons@gmail.com>
2017-05-21 22:52:17 +0300

    First Build

---
+++ Assets/_Code/GameController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+
+    private Animator animator;
+
+    public int Speed;
+
+
+    private float translation;
+
+	void Start () {
+
+        animator = this.gameObject.GetComponent<Animator>();
+
+    }
+	
+	void Update ()
+    {
+
+
+        if (Input.GetButton("Horizontal"))
+        {
+            translation = Input.GetAxis("Horizontal") * Speed * Time.deltaTime;
+
+            if (translation < 0)
+            {
+                animator.SetBool("Idle_01", false);
+                animator.SetBool("Run_02", true);
+                transform.localScale = new Vector2(1, 1);
+            }
+            else if (translation > 0)
+            {
+                transform.localScale = new Vector2(-1, 1);
+                animator.SetBool("Run_02", true);
+                animator.SetBool("Idle_01", false);
+            }
+            
+            transform.position += new Vector3(translation, 0, 0);
+        }
+        else
+        {
+            animator.SetBool("Run_02", false);
+            animator.SetBool("Idle_01", true);
+        }
+
+    }
+}
---
+++ Assets/_Code/Level Manager/InstaKill.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InstaKill : MonoBehaviour {
+
+	public void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "player")
+        {
+            print("Damage taken");
+        }
+    }
+}

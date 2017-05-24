2e48c9e13426b0e0d09a8e002e0b23b2a4865818
Anestis Konstantinidis <anestiskons@gmail.com>
2017-05-24 14:31:23 +0300

    Hadle Data

--- Assets/_Code/HandleData.cs
+++ Assets/_Code/HandleData.cs
@@ -1,19 +1,52 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class HandleData : MonoBehaviour {
 
-	// Use this for initialization
+
+    public Transform player;
+
+
+    private List<State> state;
+    private State currentState;
+	
 	void Start () {
-		
-	}
+
+        state = new List<State>();
+        //currentState = new State();
+        StartCoroutine(WriteData());
+    }
 	
 	
     
     IEnumerator WriteData()
     {
+        while (true)
+        {
 
-        yield return new WaitForSeconds(0.1f);
+            //currentState.position = new Vector2(player.position.x, player.position.y);
+            //currentState.time = Time.time;
+
+            state.Add(new State(player.position, Time.time));
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+    
+
+}
+
+[Serializable]
+public class State
+{
+    public Vector2 position;
+    public float time;
+
+    public State(Vector2 pos, float t)
+    {
+        position = pos;
+        time = t;
     }
 }
